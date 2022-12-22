e2d74b57d3751baf6d56e417e6690c23a5e9752f
Esteban Hidalgo <estebanhidalgo@Estebans-MacBook-Air.local>
2022-12-22 17:11:00 -0500

    feat: added missing features for the redux global state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import './App.css';
+import { useAppSelector, useAppDispatch } from './hooks/redux_hooks';
+import { gameActions } from './store';
 
 function App() {
-  const player = useSelector((state: number) => {
-    if (state.currentPlayer !== 'number') {
-      console.log('error');
-    }
-
-    return state.currentPlayer;
-  });
-  return <div className='App'></div>;
+  const currentPlayer = useAppSelector((state) => state.game.currentPlayer);
+  const currentScores = useAppSelector((state) => state.game.scores);
+  const dispatch = useAppDispatch();
+  return (
+    <div className='App'>
+      <h1>{currentPlayer}</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,11 +1,11 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface initialGameStateInterface {
+interface InitialGameStateInterface {
   currentPlayer: 0 | 1;
-  scores: any[];
+  scores: number[][];
 }
 
-const initialState: initialGameStateInterface = {
+const initialState: InitialGameStateInterface = {
   currentPlayer: 0,
   scores: [[], []],
 };
@@ -14,8 +14,12 @@ const gameStoreSlice = createSlice({
   name: 'gameStore',
   initialState,
   reducers: {
-    togglePlayer(state) {
+    togglePlayer(state: InitialGameStateInterface) {
       state.currentPlayer = state.currentPlayer === 0 ? 1 : 0;
+    },
+
+    addBoxSelection(state, action: PayloadAction<number>) {
+      state.scores[state.currentPlayer].push(action.payload);
     },
   },
 });
@@ -25,6 +29,11 @@ const store = configureStore({
     game: gameStoreSlice.reducer,
   },
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
 
 export const gameActions = gameStoreSlice.actions;
 
